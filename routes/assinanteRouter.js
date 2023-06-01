e70f316ce307ea157719ce1b3b07a699998311fe
DiogoStocco <102595422+DiogoStocco@users.noreply.github.com>
2023-05-31 21:30:14 -0300

    Add files via upload

--- routes/assinanteRouter.js
+++ routes/assinanteRouter.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const assinanteController = require('../controllers/assinanteController');
-const multer = require('multer'); // Pacote para lidar com o upload de arquivos
+const multer = require('multer');
 
 const upload = multer();
 
 router.get('/listar', assinanteController.listar);
 router.get('/buscarUsuarios', assinanteController.buscarUsuarios);
-router.post('/criar', upload.single('imagem'), assinanteController.salvar);
+router.get('/listarPorNome/:Nome', assinanteController.listarPorNome);
+router.get('/listarPorSobrenome', assinanteController.listarPorSobrenome);
+router.get('/listarPorCidade', assinanteController.listarPorCidade);
+router.get('/listarPorEstado', assinanteController.listarPorEstado);
+router.get('/listarPorStatus', assinanteController.listarPorStatus);
+router.post('/criar', upload.single('ImagemPerfil'), assinanteController.salvar);
 router.get('/buscarPorId/:id', assinanteController.buscarPorId);
-router.put('/atualizar/:id', assinanteController.atualizar);
+router.put('/atualizar/:id',upload.single('ImagemPerfil'), assinanteController.atualizar);
 router.delete('/excluir/:id', assinanteController.excluir);
 
 module.exports = router;
-
